ee0c40e070c093ed06f9445c1e3489632cdb343e
Kory Draughn <korydraughn@renci.org>
2023-08-06 10:27:11 -0400

    code change. logger

--- src/log.hpp
+++ src/log.hpp
@@ -40,37 +40,37 @@ namespace irods::http::log
     } // trace
 
     template <typename... Args>
-    constexpr auto trace(fmt::format_string<Args...> _format, Args&&... _args) -> void
+    constexpr auto trace(const std::string_view _format, Args&&... _args) -> void
     {
         spdlog::trace(_format, std::forward<Args>(_args)...);
     } // trace
 
     template <typename ...Args>
-    constexpr auto info(fmt::format_string<Args...> _format, Args&&... _args) -> void
+    constexpr auto info(const std::string_view _format, Args&&... _args) -> void
     {
         spdlog::info(_format, std::forward<Args>(_args)...);
     } // info
 
     template <typename ...Args>
-    constexpr auto debug(fmt::format_string<Args...> _format, Args&&... _args) -> void
+    constexpr auto debug(const std::string_view _format, Args&&... _args) -> void
     {
         spdlog::debug(_format, std::forward<Args>(_args)...);
     } // debug
 
     template <typename ...Args>
-    constexpr auto warn(fmt::format_string<Args...> _format, Args&&... _args) -> void
+    constexpr auto warn(const std::string_view _format, Args&&... _args) -> void
     {
         spdlog::warn(_format, std::forward<Args>(_args)...);
     } // warn
 
     template <typename ...Args>
-    constexpr auto error(fmt::format_string<Args...> _format, Args&&... _args) -> void
+    constexpr auto error(const std::string_view _format, Args&&... _args) -> void
     {
         spdlog::error(_format, std::forward<Args>(_args)...);
     } // error
 
     template <typename ...Args>
-    constexpr auto critical(fmt::format_string<Args...> _format, Args&&... _args) -> void
+    constexpr auto critical(const std::string_view _format, Args&&... _args) -> void
     {
         spdlog::critical(_format, std::forward<Args>(_args)...);
     } // critical
