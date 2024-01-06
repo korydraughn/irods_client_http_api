536a44b03f6a12ee0c730af4329d460cd74cf074
Kory Draughn <korydraughn@renci.org>
2024-01-06 02:11:31 -0500

    [?] Guarantee calls to std::strncpy leave space for null terminator.

--- endpoints/data_objects/src/main.cpp
+++ endpoints/data_objects/src/main.cpp
@@ -1294,7 +1294,7 @@ namespace
 					irods::at_scope_exit free_memory{[&input] { clearKeyVal(&input.condInput); }};
 
 					if (const auto iter = _args.find("lpath"); iter != std::end(_args)) {
-						std::strncpy(input.objPath, iter->second.c_str(), sizeof(DataObjInp::objPath));
+						std::strncpy(input.objPath, iter->second.c_str(), sizeof(DataObjInp::objPath) - 1);
 					}
 					else {
 						log::error("{}: Missing [lpath] parameter.", fn);
@@ -1374,7 +1374,7 @@ namespace
 					irods::at_scope_exit free_memory{[&input] { clearKeyVal(&input.condInput); }};
 
 					if (const auto iter = _args.find("lpath"); iter != std::end(_args)) {
-						std::strncpy(input.objPath, iter->second.c_str(), sizeof(DataObjInp::objPath));
+						std::strncpy(input.objPath, iter->second.c_str(), sizeof(DataObjInp::objPath) - 1);
 					}
 					else {
 						log::error("{}: Missing [lpath] parameter.", fn);
@@ -1752,7 +1752,7 @@ namespace
 				DataObjInp input{};
 				irods::at_scope_exit free_memory{[&input] { clearKeyVal(&input.condInput); }};
 
-				std::strncpy(input.objPath, lpath_iter->second.c_str(), sizeof(DataObjInp::objPath));
+				std::strncpy(input.objPath, lpath_iter->second.c_str(), sizeof(DataObjInp::objPath) - 1);
 
 				if (catalog_only_iter->second == "1") {
 					input.oprType = UNREG_OPR;
@@ -2085,7 +2085,7 @@ namespace
 					irods::at_scope_exit free_memory{[&input] { clearKeyVal(&input.condInput); }};
 
 					if (const auto iter = _args.find("lpath"); iter != std::end(_args)) {
-						std::strncpy(input.objPath, iter->second.c_str(), sizeof(DataObjInp::objPath));
+						std::strncpy(input.objPath, iter->second.c_str(), sizeof(DataObjInp::objPath) - 1);
 					}
 					else {
 						log::error("{}: Missing [lpath] parameter.", fn);
@@ -2172,7 +2172,7 @@ namespace
 					irods::at_scope_exit free_memory{[&input] { clearKeyVal(&input.condInput); }};
 
 					if (const auto iter = _args.find("lpath"); iter != std::end(_args)) {
-						std::strncpy(input.objPath, iter->second.c_str(), sizeof(DataObjInp::objPath));
+						std::strncpy(input.objPath, iter->second.c_str(), sizeof(DataObjInp::objPath) - 1);
 					}
 					else {
 						log::error("{}: Missing [lpath] parameter.", fn);
@@ -2280,7 +2280,7 @@ namespace
 				irods::at_scope_exit free_memory{[&info] { clearKeyVal(&info.condInput); }};
 
 				if (const auto iter = _args.find("lpath"); iter != std::end(_args)) {
-					std::strncpy(info.objPath, iter->second.c_str(), sizeof(DataObjInfo::objPath));
+					std::strncpy(info.objPath, iter->second.c_str(), sizeof(DataObjInfo::objPath) - 1);
 				}
 				else {
 					log::error("{}: Missing [lpath] parameter.", fn);
@@ -2288,7 +2288,7 @@ namespace
 				}
 
 				if (auto iter = _args.find("resource-hierarchy"); iter != std::end(_args)) {
-					std::strncpy(info.rescHier, iter->second.c_str(), sizeof(DataObjInfo::rescHier));
+					std::strncpy(info.rescHier, iter->second.c_str(), sizeof(DataObjInfo::rescHier) - 1);
 				}
 				else if (iter = _args.find("replica-number"); iter != std::end(_args)) {
 					info.replNum = std::stoi(iter->second);
